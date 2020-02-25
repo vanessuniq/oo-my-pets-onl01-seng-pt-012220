132621af6ae6523b9c33e62d1fccecc3ae8c779b
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-25 04:02:11 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,16 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  @@all = []
+  
+  def initialize (name, owner)
+    @name = name
+    @owner = Owner.new(owner)
+    @mood = 'nervous'
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
+  
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,32 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species 
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @species = 'human'
+    @@all << self
+  end
+  
+  def say_species
+    puts "I am a #{@species}."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    all.count
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def cats
+    Cat.all.select {|cat| cat.owner == self}
+  end
+  
+  
 end
